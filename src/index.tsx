d20dd78fab1db502e4c5279e5287b6fd52d7715b
malenarmr <maalena456@gmail.com>
2025-05-09 15:57:33 -0300

    Resete archivo index por error en vercel

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,13 @@
-import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "antd/dist/antd.min.css";
+import ReactDOM from "react-dom";
+import 'antd/dist/antd.min.css';
+
 import Router from "./router";
 
 const App = () => (
   <BrowserRouter>
-    <Router />
+      <Router />
   </BrowserRouter>
 );
 
-// Obtener el elemento contenedor
-const container = document.getElementById("root");
-
-// Verificar que el contenedor existe antes de crear la raíz
-if (container) {
-  // Crear la raíz de React
-  const root = createRoot(container);
-
-  // Renderizar la aplicación
-  root.render(<App />);
-}
+ReactDOM.render(<App />, document.getElementById("root"));
